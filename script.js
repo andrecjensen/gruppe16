84cb2b66743f440e3cfe1f222a0b2bbeb469855f
André Jensen <Jensens@laptop-a4gi1svs.kea.local>
2018-11-15 10:03:57 +0100

    udfyldning af javascript. Nået til startStory

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ console.log("Spillet som er udarbejdet af Gruppe 16");
 
 window.addEventListener("load", showStart);
 
+let showSettingsSFXSound = true;
+let showSettingsMusic = true;
+
 function sidenVises() {
     console.log("Siden er loadet");
     showStart();
@@ -25,52 +28,116 @@ function showStart() {
 function showSettings() {
     console.log("Indstillinger vises");
 
-    document.querySelector("#settings").classList.remove("hide");
+    // De to QSE herunder toggler hide class fra på settingsskærm og toggler blur class til på startskærm
+    document.querySelector("#settings").classList.toggle("hide");
+    document.querySelector("#start").classList.toggle("blur");
 
-    // FORTSÆT HERFRA §§§§§§§§§§§§§§§§
-
-    document.querySelector(".close").addEventListener("click", hideSettings);
+    // LUK-KNAP OG TOGGLES
+    document.querySelector("#toggle_sound").addEventListener("click", toggleSound);
+    document.querySelector("#toggle_music").addEventListener("click", toggleMusic);
+    document.querySelector("#close_settings").addEventListener("click", hideSettings);
 }
 
 function hideSettings() {
     console.log("Indstillinger skjules");
+
+    // De to QSE herunder toggler hide class til på settingsskærm og toggler blur class fra på startskærm
+    document.querySelector("#settings").classList.toggle("hide");
+    document.querySelector("#start").classList.toggle("blur");
 }
+
+// SFX toggle funktioner
 
 function toggleSound() {
     console.log("Toggle på SFX lyde");
 
+    // ## DER SKAL TILFØJES DOC QSE TIL SOUNDON OG SOUNDOFF
+
+    if (showSettingsSFXSound == false) {
+        //her slår vi SFX lyde på
+        showSettingsSFXSound = true;
+        soundsOn();
+
+    } else {
+        //her slår vi SFX lyde fra
+        showSettingsSFXSound = false;
+        soundsOff();
+    }
 }
 
 function soundsOff() {
-    console.log("SFX lyde slået fra");
+    console.log("SFX lyde slået fra. soundsOff funktionens værdi er " + showSettingsSFXSound);
+
+    // SFX lyde slås fra
+    document.querySelector(".sfx").muted = true;
 }
 
 function soundsOn() {
-    console.log("SFX lyde slået til");
+    console.log("SFX lyde slået til. soundsOn funktionens værdi er " + showSettingsSFXSound);
 
+    // SFX lyde slås til
+    document.querySelector(".sfx").muted = false;
 }
+
+// Musik toggle funktioner
 
 function toggleMusic() {
     console.log("Toggle på Musik");
 
+    // ## DER SKAL TILFØJES DOC QSE TIL MUSICON OG MUSICOFF
+
+    if (showSettingsMusic == false) {
+        // her slår vi Musik på
+        showSettingsMusic = true;
+        musicOn();
+
+    } else {
+        // her slår vi musik fra
+        showSettingsMusic = false;
+        musicOff();
+    }
 }
 
 function musicOff() {
-    console.log("Musik slået fra");
+    console.log("Musik slået fra. musicOff funktionens værdi er " + showSettingsMusic);
 
+    // Musik slås fra
+    document.querySelector("#music").muted = true;
 }
 
 function musicOn() {
-    console.log("Musik slået til");
+    console.log("Musik slået til. musicOn funktionens værdi er " + showSettingsMusic);
 
+    // Musik slås til
+    document.querySelector("#music").muted = false;
 }
+
+// Toggle funktioner slut
 
 function hideStart() {
     console.log("Skjuler startskærmen");
+
+    // Denne QSE vælger startlyden og spiller den
+    document.querySelector("#startsound").play();
+
+    // Denne QSE vælger startknappen og fjerner dens animation, så ikke spillet bliver for tungt. Man kommer til at se den mens man spiller alligevel.
+    document.querySelector("#start .start_knap").classList.remove("pulse");
+
+    // Denne QSE starter fade animationen på startskærmen
+    document.querySelector("#start").classList.add("hide_start");
+
+    // Denne QSE viderefører til startStory når fade animationen er færdigt
+    document.querySelector("#start").addEventListener("animationend", startStory);
 }
 
 function startStory() {
     console.log("Starter historien");
+
+    // Her skal indsættes forskellige QSE der vælger figure der skal animeres
+
+    document.querySelector("#pigekjole").classList.add("pige_animation");
+    document.querySelector("#vind").classList.add("vind_animation");
+    document.querySelector("#silhuetter").classList.add("silhuetter_animation");
 }
 
 function startGame() {
